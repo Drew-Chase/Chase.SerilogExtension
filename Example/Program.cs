21c2516d6b42871ec02ce8cf27392da1f58a95f4
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-05-18 19:01:27 -0400

    Add new Program.cs with logging configuration
    The new Program.cs file has been added with extensive Serilog configuration including verbose level logging, log archiving, flushing intervals, size constraints, and automatic logger close and flush on application exit. It also includes some example log statements.

---
+++ Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using Chase.SerilogExtension;
+using Serilog;
+
+namespace Example;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .EnhancedWriteToFile(
+                directory: "logs", // The directory to write the log files to
+                archiveOldLogs: true, // Whether to archive old logs
+                flushToDiskInterval: TimeSpan.FromSeconds(30), // The interval to flush logs to disk, this will also set buffered to true
+                maxLogSize: 10485760) // This will automatically archive the log file if it exceeds 10MB
+            .AutoCloseAndFlush() // This will automatically close and flush the logger when the application exits
+            .CreateLogger();
+
+        // Writes to debug.log
+        Log.Verbose("hello world");
+        Log.Debug("hello world");
+
+        // Writes to latest.log
+        Log.Information("hello world");
+        Log.Warning("hello world");
+
+        // Writes to error.log
+        Log.Error("hello world");
+        Log.Fatal("hello world");
+    }
+}
